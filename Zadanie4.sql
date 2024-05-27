fe985373b0d1558553fe934b7fb56d7bc7fc66e7
devozik <166386951+devozik@users.noreply.github.com>
2024-05-27 08:49:52 +0200

    Update Zadanie4.sql

--- Zadanie4.sql
+++ Zadanie4.sql
@@ -9,10 +9,11 @@ order by contact_id;
 -- Rozwiązanie #2 ze wszystkimi kolumnami
 
 select distinct
-    last_value(contact_id) over (partition by client_id order by contact_timestamp range between unbounded preceding and unbounded following) as contact_id,
+    last_value(contact_id) over w as contact_id,
     client_id,
-    last_value(agent_id) over (partition by client_id order by contact_timestamp range between unbounded preceding and unbounded following) as agent_id,
-    last_value(status) over (partition by client_id order by contact_timestamp range between unbounded preceding and unbounded following) as status,
-    last_value(contact_timestamp) over (partition by client_id order by contact_timestamp range between unbounded preceding and unbounded following) as contact_timestamp
+    last_value(agent_id) over w as agent_id,
+    last_value(status) over w as status,
+    last_value(contact_timestamp) over w as contact_timestamp
 from contacts
+window w as (partition by client_id order by contact_timestamp range between unbounded preceding and unbounded following)
 order by contact_id;
